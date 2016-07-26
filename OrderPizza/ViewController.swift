a870bbeedf0422a7889da7e06a65ffaa19b07b3b
Mario Reyes Ojeda <mfreyesojeda@gmail.com>
2016-07-25 22:54:13 -0500

    Create the views in Storyboard

--- OrderPizza/ViewController.swift
+++ OrderPizza/ViewController.swift
@@ -8,10 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    let sizePizzaData = ["Small", "Medium", "Large"]
+    var sizeItem:String?
+    
+    @IBOutlet weak var sizePizzaPicker: UIPickerView!
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return sizePizzaData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return sizePizzaData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        sizeItem = sizePizzaData[sizePizzaPicker.selectedRowInComponent(0)]
+    }
+    
+    @IBAction func sizeNextButton(sender: AnyObject) {
+        
+        print("The user selected: \(sizeItem)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        sizePizzaPicker.delegate = self
+        sizePizzaPicker.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
